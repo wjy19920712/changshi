d0f47df2b8a1cb24c22c5a0c99a6c2ab0369cd02
wjy <jian199207@163.com>
2017-03-11 10:55:17 +0800

    第五次提交

--- src/com/turing/gittest.java
+++ src/com/turing/gittest.java
@@ -3,5 +3,6 @@ package com.turing;
 public class gittest {
 	public static void main(String[] args) {
 		System.out.println("xxxxxxxxxxxxxxxxxxxx");
+		System.out.println("xxxxxxxxxxxxxxxxxxxx");
 	}
 }
